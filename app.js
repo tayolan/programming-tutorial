595496502ac36aed52028b07170069eb2bf612b6
tayolan <tayolan@yahoo.com>
2022-03-04 20:11:04 +0100

    push for Lesson3

---
+++ app.js
@@ -0,0 +1,25 @@
+// function showMessage(name) {
+//     alert( `Hello ` + name + `how are you doing` );
+//   }
+  
+//   showMessage("tayo");
+//   showMessage("clement");
+
+
+
+
+//   let year = prompt('in which year was the ECMAScript-2015 specification published?',"");
+
+//   if (year < 2015) {
+//       alert('Too early..');
+// }else if (year > 2015) {
+//           alert('Too late');
+//         }else if (year == 2015) {
+//             alert ('correct')
+//         }
+
+let i = 0;
+while (i < 3) {//shows 0, then 1, then 2
+    alert (i);
+    i++;
+}
